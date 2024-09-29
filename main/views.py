30263546ccb70540a3d955a50ccc0a08f3e739c0
Abhinav sahu <163006809+iabhinavsahu@users.noreply.github.com>
2024-09-29 17:46:18 +0530

    Delete main directory

--- main/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render,HttpResponse
-
-# Create your views here.
-def home(request):
-    return HttpResponse("Home page")
-
-def about(request):
-    return HttpResponse("About")
