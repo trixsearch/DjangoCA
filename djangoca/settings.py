57dde9f48b619cc28224aec54c3ca3cb3a250ad4
trixsearch <inforatme@gmail.com>
2024-09-29 17:26:10 +0530

    new changes

--- djangoca/settings.py
+++ djangoca/settings.py
@@ -119,6 +119,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field .
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
